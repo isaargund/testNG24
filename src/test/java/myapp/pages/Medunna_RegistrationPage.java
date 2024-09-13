73f3caa8cffa691e6fbd5648a911ae27608786d4
isa <ode121@gmail.com>
2024-09-13 18:40:58 +0200

    6

--- src/test/java/myapp/pages/Medunna_RegistrationPage.java
+++ src/test/java/myapp/pages/Medunna_RegistrationPage.java
@@ -9,39 +9,31 @@ public class Medunna_RegistrationPage {
     public Medunna_RegistrationPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    @FindBy(id = "ssn")
+    @FindBy (id = "ssn")
     public WebElement ssn;
-
     @FindBy (xpath = "//div[text()='Your SSN is invalid']")
     public WebElement invalidSsnMessage;
-
     @FindBy (xpath = "//div[text()='Your SSN is required.']")
     public WebElement ssnRequiredMessage;
-
     @FindBy(id = "firstName")
     public WebElement firstName;
-
-    @FindBy(id = "lastName")
-    public WebElement lastName;
-
     @FindBy (xpath = "//div[text()='Your FirstName is required.']")
     public WebElement firstNameRequiredMessage;
-
+    @FindBy(id = "lastName")
+    public WebElement lastName;
     @FindBy (xpath = "//div[text()='Your LastName is required.']")
     public WebElement lastNameRequiredMessage;
-
-    @FindBy(id = "username")
-    public WebElement Username;
-
     @FindBy (id = "username")
     public WebElement username;
     @FindBy (xpath = "//div[text()='Your username is required.']")
     public WebElement userNameRequiredMessage;
     @FindBy (xpath = "//div[text()='Your username is invalid.']")
     public WebElement userNameInvalidMessage;
-
-
-
-
+    @FindBy (id = "email")
+    public WebElement email;
+    @FindBy (xpath = "//div[text()='This field is invalid']")
+    public WebElement emailInvalidMessage;
+    @FindBy (xpath = "//div[text()='Your email is required.']")
+    public WebElement emailRequiredMessage;
 
 }
--- src/test/java/myapp/tests/medunna_practice/Day24_US002.java
+++ src/test/java/myapp/tests/medunna_practice/Day24_US002.java
@@ -7,6 +7,7 @@ import myapp.utilities.ConfigReader;
 import myapp.utilities.Driver;
 import myapp.utilities.WaitUtils;
 import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -18,16 +19,15 @@ public class Day24_US002 {
             US002AC01TC01: User leaves the username blank, there should be "Your username is required." error message
             US002AC01TC02: User enters username special chars(£#$€), there should be "Your username is invalid." error message
             US002AC01TC03: User enters username  that can contain any chars, there should not be any error message
-        US002AC02: username should be validated using api and must be unique
-        US002AC03: email id cannot be created without "@" sign and "." extension
-            US002AC03TC01: User enters the email as "johndoe.com", there should be "This field is invalid" error message
-            US002AC03TC02: User enters the email as "john@doecom", there should be "This field is invalid" error message
-            US002AC03TC03: User enters the email as "john@doe.", there should be "This field is invalid" error message
-            US002AC03TC04: User enters the email as "@doe.com", there should be "This field is invalid" error message
+        US002AC02: email id cannot be created without "@" sign and "." extension
+            US002AC02TC01: User enters the email as "johndoe.com", there should be "This field is invalid" error message
+            US002AC02TC02: User enters the email as "john@doecom", there should be "This field is invalid" error message
+            US002AC02TC03: User enters the email as "john@doe.", there should be "This field is invalid" error message
+            US002AC02TC04: User enters the email as "@doe.com", there should be "This field is invalid" error message
             US002AC03TC05: User enters the email as "john@doe.com", there should not be any error message
-        US002AC04: email cannot be left blank
-            US002AC04TC01: User leaves the email blank, there should be "Your email is required." error message
-            US002AC04TC02: User enters the email as "john@doe.com", there should not be any error message
+        US002AC03: email cannot be left blank
+            US002AC03TC01: User leaves the email blank, there should be "Your email is required." error message
+            US002AC03TC02: User enters the email as "john@doe.com", there should not be any error message
      */
     Medunna_HomePage medunnaHomePage = new Medunna_HomePage();
     Medunna_RegistrationPage medunnaRegistrationPage =  new Medunna_RegistrationPage();
@@ -37,6 +37,11 @@ public class Day24_US002 {
         Driver.getDriver().get(ConfigReader.getProperty("medunna_url"));
         BrowserUtils.clickWithTimeOut(medunnaHomePage.userIcon, 2);
         BrowserUtils.clickWithTimeOut(medunnaHomePage.registerOption, 2);
+    }
+
+    @AfterTest
+    public void closeDriver(){
+        Driver.closeDriver();
     }
 
     //US002AC01: Choose a username that can contain any chars, but it cannot be blank
@@ -51,12 +56,58 @@ public class Day24_US002 {
         BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.userNameInvalidMessage);
         WaitUtils.waitFor(1);
         //US002AC01TC03: User enters username  that can contain any chars, there should not be any error message
+        medunnaRegistrationPage.username.clear();
         medunnaRegistrationPage.username.sendKeys("Techpro",Keys.TAB);
         BrowserUtils.verifyElementNotDisplayed(medunnaRegistrationPage.userNameRequiredMessage);
         BrowserUtils.verifyElementNotDisplayed(medunnaRegistrationPage.userNameInvalidMessage);
         WaitUtils.waitFor(1);
     }
 
+    // US002AC02: email id cannot be created without "@" sign and "." extension
+    @Test
+    public void medunna2(){
+        //US002AC02TC01: User enters the email as "johndoe.com", there should be "This field is invalid" error message
+        medunnaRegistrationPage.email.sendKeys("johndoe.com",Keys.TAB);
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        WaitUtils.waitFor(1);
+        //US002AC02TC02: User enters the email as "john@doecom", there should be "This field is invalid" error message
+        medunnaRegistrationPage.email.clear();
+        medunnaRegistrationPage.email.sendKeys("john@doecom", Keys.TAB);
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        WaitUtils.waitFor(1);
+        //US002AC02TC03: User enters the email as "john@doe.", there should be "This field is invalid" error message
+        medunnaRegistrationPage.email.clear();
+        medunnaRegistrationPage.email.sendKeys("john@doe.", Keys.TAB);
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        WaitUtils.waitFor(1);
+        //US002AC02TC04: User enters the email as "@doe.com", there should be "This field is invalid" error message
+        medunnaRegistrationPage.email.clear();
+        medunnaRegistrationPage.email.sendKeys("@doe.com", Keys.TAB);
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        WaitUtils.waitFor(1);
+        //US002AC03TC05: User enters the email as "john@doe.com", there should not be any error message
+        medunnaRegistrationPage.email.clear();
+        medunnaRegistrationPage.email.sendKeys("john@doe.com", Keys.TAB);
+        BrowserUtils.verifyElementNotDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        WaitUtils.waitFor(1);
+    }
+
+    //US002AC03: email cannot be left blank
+    @Test
+    public void medunna3(){
+        //US002AC03TC01: User leaves the email blank, there should be "Your email is required." error message
+        WaitUtils.waitFor(3);
+        medunnaRegistrationPage.email.clear();
+        medunnaRegistrationPage.email.sendKeys("", Keys.ENTER);
+        WaitUtils.waitFor(1);
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.emailRequiredMessage);
+        WaitUtils.waitFor(1);
+        //US002AC03TC02: User enters the email as "john@doe.com", there should not be any error message
+        medunnaRegistrationPage.email.sendKeys("john@doe.com", Keys.TAB);
+        BrowserUtils.verifyElementNotDisplayed(medunnaRegistrationPage.emailInvalidMessage);
+        //  BrowserUtils.verifyElementNotDisplayed(medunnaRegistrationPage.emailRequiredMessage);
+        WaitUtils.waitFor(1);
+    }
 }
 
 
